a7cf15f1be2e160b9effd5e0198905ebd8c6d0cc
lehoangnam26102000 <lehoangnam@26102000@gmail.com>
2022-06-18 14:37:28 +0700

    Update: AddComment

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hungry/views/screens/auth/welcome_page.dart';
 
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    systemNavigationBarColor: Colors.black,
-    systemNavigationBarIconBrightness: Brightness.light,
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyAaXc4aT0YSI7afshSAo7-uR2RBH_7MyJc",
+      appId: "1:820904930063:android:6e2684c718a01f2fc7ac7a",
+      messagingSenderId: "XXX",
+      projectId: "login-registration-flutter",
+    ),
+  );
   runApp(MyApp());
 }
 
